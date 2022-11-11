9801a2c99ca4c751e268060b19e266f20969d6a5
Adam <424778940z@users.noreply.github.com>
2022-11-11 21:02:51 +0800

    CI configuration update and minor code structure change (#4)
    * move firmware info to seprate file
    
    * update gitignore
    
    * update ci config

---
+++ app/firmware_config.h
@@ -0,0 +1,14 @@
+#ifndef FIRMWARE_CONFIG
+#define FIRMWARE_CONFIG
+
+#define MANUFACTURER_NAME               "OneKey"                                     /**< Manufacturer. Will be passed to Device Information Service. */
+#define ADV_HEAD_NAME                   "Touch "
+#define MODEL_NUMBER                    "Touch"                                       /**< Model Number string. Will be passed to Device Information Service. */
+#define MANUFACTURER_ID                 0x55AA55AA55                                /**< DUMMY Manufacturer ID. Will be passed to Device Information Service. You shall use the ID for your Company*/
+#define ORG_UNIQUE_ID                   0xEEBBEE                                    /**< DUMMY Organisation Unique ID. Will be passed to Device Information Service. You shall use the Organisation Unique ID relevant for your Company */
+#define HW_REVISION                     "1.0.0"
+#define FW_REVISION                     "2.1.0"
+#define SW_REVISION                     "s132_nrf52_7.0.1"
+#define BT_REVISION                     "1.0.1"
+
+#endif //FIRMWARE_CONFIG
