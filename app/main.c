fd7c6ebd50450d4e6ca7acfce885788366834f9e
Adam BZH <424778940z@users.noreply.github.com>
2022-11-11 14:44:04 +0800

    move firmware info to seprate file
    Signed-off-by: Adam BZH <424778940z@users.noreply.github.com>

--- app/main.c
+++ app/main.c
@@ -104,15 +104,7 @@
 #include "nrf_uarte.h"
 #endif
 
-#define MANUFACTURER_NAME               "OneKey"                                     /**< Manufacturer. Will be passed to Device Information Service. */
-#define ADV_HEAD_NAME                   "Touch "
-#define MODEL_NUMBER                    "Touch"                                       /**< Model Number string. Will be passed to Device Information Service. */
-#define MANUFACTURER_ID                 0x55AA55AA55                                /**< DUMMY Manufacturer ID. Will be passed to Device Information Service. You shall use the ID for your Company*/
-#define ORG_UNIQUE_ID                   0xEEBBEE                                    /**< DUMMY Organisation Unique ID. Will be passed to Device Information Service. You shall use the Organisation Unique ID relevant for your Company */
-#define HW_REVISION                     "1.0.0"
-#define FW_REVISION                     "2.1.0"
-#define SW_REVISION                     "s132_nrf52_7.0.1"
-#define BT_REVISION                     "1.0.1"
+#include "firmware_config.h"
 
 #define APDU_TAG_BLE                    0x44
 
